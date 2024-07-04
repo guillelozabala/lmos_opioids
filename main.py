8a668f48a3e11e569a8ef23183c08a874312d66e
Guillermo Martínez <guillelozabala@gmail.com>
2024-07-04 16:23:20 +0200

    wage distributions

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ folder_path = os.getcwd()
 file_path_demographics = os.path.join(folder_path, 'code\\data_construction\\', 'demographics.py')
 file_path_labor_market = os.path.join(folder_path, 'code\\data_construction\\', 'labor_market_outcomes.py')
 file_path_sector_comp = os.path.join(folder_path, 'code\\data_construction\\', 'sector_composition.py')
+file_path_min_wage = os.path.join(folder_path, 'code\\data_construction\\', 'minwage_cleaning.py')
 
 # Run the scripts
 subprocess.run(['python', file_path_demographics])
 subprocess.run(['python', file_path_labor_market])
 subprocess.run(['python', file_path_sector_comp])
+subprocess.run(['python', file_path_min_wage])
